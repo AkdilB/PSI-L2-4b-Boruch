2e999e5b6e1ff7f200f10107c82d89862ddbfc01
AkdilB_github <lidiaboruch8@gmail.com_github>
2026-01-15 10:56:31 +0100

    “moj-drugi-commit”

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 var i = 0;
 function zmiana(btn) {
-    if (i == 1) {
+    if (i == 1)
+    {
         btn.style.backgroundColor = "lightblue";
         i = 0;
     }
-    else if (i == 2){
+    else if (i == 2)
+    {
         btn.style.backgroundColor = "lightgreen";
         i = 1
     }
-    else {
+    else 
+    {
         btn.style.backgroundColor = "lightgrey";
         i = 2
     }
